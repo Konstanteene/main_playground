9588fd3f09024a079d9d71de6998909f89bff34e
Kostiantyn__ <kostyasend02@gmail.com>
2023-12-24 20:18:55 +0200

    nodejs

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+app.use(express.static(path.join(__dirname, 'pages')));
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'pages', 'home','index.html'));
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
+
+app.use((req, res, next) => {
+    console.log(req.url);
+    next();
+  });
