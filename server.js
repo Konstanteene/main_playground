c1fe60da0967e2fbae58c30aed31e07cff2bd3c2
Kostiantyn__ <kostyasend02@gmail.com>
2023-12-27 20:53:27 +0200

    add mysql

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require('express');
+const mysql = require('mysql2');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const PORT = 3000;
 app.use(express.static(path.join(__dirname, 'pages')));
 
+// db 
+var connection = mysql.createConnection({
+    host     : process.env.MYSQL_HOST,
+    user     : process.env.MYSQL_USER,
+    password : process.env.MYSQL_PASSWORD,
+    database : process.env.MYSQL_DATABASE 
+});
+  
+connection.connect(function(err) {
+    if (err) {
+      console.error('Error connecting: ' + err.stack);
+      return;
+    }
+    console.log('Connected');
+});
+
+// pages
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, 'pages', 'index.html'));
-})
+});
 
 app.listen(PORT, () =>{
     console.log(`Server is running on http://localhost:${PORT}`);
